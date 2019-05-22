47a94e0d753a47432b4482eca4618a99128e33f0
HP <HP@DESKTOP-6SUN5DR>
2019-05-22 14:22:07 +1200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace seventh
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 6;
+            int width = 6;
+            int height = width;
+
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    Console.Write(num);
+                }
+                Console.WriteLine();
+                width--;
+            }
+        }
+    }
+}
